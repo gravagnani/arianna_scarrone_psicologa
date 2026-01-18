7b6260cdc5161d375f48ae8413f2128962d71d7b
gravagnani <giuseppe.ravagnani@gmail.com>
2026-01-18 16:37:38 +0100

    finish pages and refactor

---
+++ src/config/paths.ts
@@ -0,0 +1,10 @@
+// src/config/paths.ts
+// Centralized path configuration for sections and navigation
+
+export const PATHS = [
+  { href: "/", text: "Chi sono" },
+  { href: "/#cosa-aspettarsi", text: "Come posso aiutarti" },
+  { href: "/#cosa-aspettarsi", text: "Cosa aspettarsi" },
+  { href: "/dove-sono", text: "Dove sono" },
+  { href: "/#faq", text: "FAQ" },
+];
