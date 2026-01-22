b2d5bedd60753e515eaa7b43c59de10713c7c2ee
gravagnani <giuseppe.ravagnani@gmail.com>
2026-01-22 21:47:42 +0100

    update location details and correct VAT number in contact information

--- src/config/contacts.ts
+++ src/config/contacts.ts
@@ -3,9 +3,9 @@
 
 export const CONTACTS = {
   name: "Arianna Scarrone",
-  vatNumber: "02860290094",
+  vatNumber: "01301270078",
   phoneText: "(+39) 353 4951121",
   phone: "+393534951121",
   email: "ariannascarrone.psicologa@gmail.com",
-  address: "Via Bernardo Gonzati 3, Padova, Italia",
+  address: "Via L. Pellizzo 39, Padova, Italia",
 };
