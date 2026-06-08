f586f51ad8700f84490a497616eecf135b239aef
gravagnani <giuseppe.ravagnani@gmail.com>
2026-06-08 22:22:50 +0200

    Enhance footer: add guida psicologi link and image; improve layout and styling

--- src/config/contacts.ts
+++ src/config/contacts.ts
@@ -13,4 +13,8 @@ export const CONTACTS = {
   city: "Padova",
   postalCode: "35137",
   country: "Italia",
+  guidaPsicologiURL:
+    "https://www.guidapsicologi.it/studio/arianna-scarrone?utm_source=204393&utm_medium=widget&utm_campaign=widget-company_stamp",
+  guidaPsicologiImageURL:
+    "https://www.guidapsicologi.it/stamp.xpng?com=204393&v=10",
 };
